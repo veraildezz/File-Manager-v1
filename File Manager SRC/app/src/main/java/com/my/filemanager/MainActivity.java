ad270b364d23da8428c10b4105fe20e02924e93b
veraildez <165566710+veraildezz@users.noreply.github.com>
2025-01-30 16:00:21 +0300

    Create MainActivity.java

---
+++ File Manager SRC/app/src/main/java/com/my/filemanager/MainActivity.java
@@ -0,0 +1,236 @@
+package com.my.filemanager;
+
+import android.Manifest;
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.*;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends Activity {
+	
+	private String file = "";
+	private String path = "";
+	private double n = 0;
+	private String UPfolder = "";
+	
+	private ArrayList<String> lists = new ArrayList<>();
+	private ArrayList<HashMap<String, Object>> listMap = new ArrayList<>();
+	
+	private LinearLayout linear1;
+	private ListView listview1;
+	
+	private SharedPreferences data;
+	private SharedPreferences files;
+	private Intent gi = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		
+		if (Build.VERSION.SDK_INT >= 23) {
+			if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+				requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+			} else {
+				initializeLogic();
+			}
+		} else {
+			initializeLogic();
+		}
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		listview1 = findViewById(R.id.listview1);
+		data = getSharedPreferences("data", Activity.MODE_PRIVATE);
+		files = getSharedPreferences("files", Activity.MODE_PRIVATE);
+		
+		listview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> _param1, View _param2, int _param3, long _param4) {
+				final int _position = _param3;
+				path = lists.get((int)(_position));
+				if (FileUtil.isDirectory(lists.get((int)(_position)))) {
+					file = lists.get((int)(_position));
+					_list();
+				}
+				else {
+					SketchwareUtil.showMessage(getApplicationContext(), Uri.parse(lists.get((int)(_position))).getLastPathSegment());
+					files.edit().putString("data", path).commit();
+					gi.setClass(getApplicationContext(), ReadFileActivity.class);
+					startActivity(gi);
+					finish();
+				}
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		file = FileUtil.getExternalStorageDir();
+		_list();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (file.equals(FileUtil.getExternalStorageDir())) {
+			finish();
+		}
+		else {
+			UPfolder = file.substring((int)(0), (int)(file.lastIndexOf("/")));
+			file = UPfolder;
+			_list();
+		}
+	}
+	public void _list() {
+		listMap.clear();
+		FileUtil.listDir(file, lists);
+		n = 0;
+		for(int _repeat14 = 0; _repeat14 < (int)(lists.size()); _repeat14++) {
+			{
+				HashMap<String, Object> _item = new HashMap<>();
+				_item.put(file, lists.get((int)(n)));
+				listMap.add(_item);
+			}
+			
+			n++;
+		}
+		listview1.setAdapter(new Listview1Adapter(listMap));
+		((BaseAdapter)listview1.getAdapter()).notifyDataSetChanged();
+	}
+	
+	public class Listview1Adapter extends BaseAdapter {
+		
+		ArrayList<HashMap<String, Object>> _data;
+		
+		public Listview1Adapter(ArrayList<HashMap<String, Object>> _arr) {
+			_data = _arr;
+		}
+		
+		@Override
+		public int getCount() {
+			return _data.size();
+		}
+		
+		@Override
+		public HashMap<String, Object> getItem(int _index) {
+			return _data.get(_index);
+		}
+		
+		@Override
+		public long getItemId(int _index) {
+			return _index;
+		}
+		
+		@Override
+		public View getView(final int _position, View _v, ViewGroup _container) {
+			LayoutInflater _inflater = getLayoutInflater();
+			View _view = _v;
+			if (_view == null) {
+				_view = _inflater.inflate(R.layout.cus, null);
+			}
+			
+			final LinearLayout linear1 = _view.findViewById(R.id.linear1);
+			final LinearLayout linear2 = _view.findViewById(R.id.linear2);
+			final ImageView imageview1 = _view.findViewById(R.id.imageview1);
+			final TextView textview1 = _view.findViewById(R.id.textview1);
+			
+			textview1.setText(Uri.parse(lists.get((int)(_position))).getLastPathSegment());
+			
+			return _view;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+          }
+      
